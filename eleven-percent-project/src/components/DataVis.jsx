56ebd5ae62fe809fe3586d4478d51a74ffa48e2a
Cazi <71891696+Cazi@users.noreply.github.com>
2023-10-25 14:46:09 -0400

    Both total student bar graphs and percentage graphs are working, and can be switched with a button and state

--- eleven-percent-project/src/components/DataVis.jsx
+++ eleven-percent-project/src/components/DataVis.jsx
@@ -1,309 +1,233 @@
 import React from 'react'
 import {
     VictoryLine, VictoryChart,
-    VictoryAxis, VictoryTheme, VictoryBar
+    VictoryAxis, VictoryTheme, VictoryBar, VictoryScatter, VictoryLabel
 } from 'victory';
+import { useState } from 'react';
 //Data
 //Total Students at Brown
-const totalStudents = [
+const totalBlackStudentsPercent = [
     {
-        Year: 1980,
-        Students: 6867
+        "Year": 1980,
+        "TOTAL STUDENTS": 6867,
+        "TOTAL BLACK STUDENTS": 382,
+        "Percentage": 5.56
     },
     {
-        Year: 1984,
-        Students: 7099
+        "Year": 1984,
+        "TOTAL STUDENTS": 7099,
+        "TOTAL BLACK STUDENTS": 429,
+        "Percentage": 6.04
     },
     {
-        Year: 1986,
-        Students: 7357
+        "Year": 1986,
+        "TOTAL STUDENTS": 7357,
+        "TOTAL BLACK STUDENTS": 464,
+        "Percentage": 6.31
     },
     {
-        Year: 1988,
-        Students: 7612
+        "Year": 1988,
+        "TOTAL STUDENTS": 7612,
+        "TOTAL BLACK STUDENTS": 460,
+        "Percentage": 6.04
     },
     {
-        Year: 1990,
-        Students: 7577
+        "Year": 1990,
+        "TOTAL STUDENTS": 7577,
+        "TOTAL BLACK STUDENTS": 446,
+        "Percentage": 5.89
     },
     {
-        Year: 1991,
-        Students: 7532
+        "Year": 1991,
+        "TOTAL STUDENTS": 7532,
+        "TOTAL BLACK STUDENTS": 436,
+        "Percentage": 5.79
     },
     {
-        Year: 1992,
-        Students: 7593
+        "Year": 1992,
+        "TOTAL STUDENTS": 7593,
+        "TOTAL BLACK STUDENTS": 443,
+        "Percentage": 5.83
     },
     {
-        Year: 1993,
-        Students: 7655
+        "Year": 1993,
+        "TOTAL STUDENTS": 7655,
+        "TOTAL BLACK STUDENTS": 424,
+        "Percentage": 5.54
     },
     {
-        Year: 1994,
-        Students: 7801
+        "Year": 1994,
+        "TOTAL STUDENTS": 7801,
+        "TOTAL BLACK STUDENTS": 436,
+        "Percentage": 5.59
     },
     {
-        Year: 1995,
-        Students: 7641
+        "Year": 1995,
+        "TOTAL STUDENTS": 7641,
+        "TOTAL BLACK STUDENTS": 444,
+        "Percentage": 5.81
     },
     {
-        Year: 1996,
-        Students: 7626
+        "Year": 1996,
+        "TOTAL STUDENTS": 7626,
+        "TOTAL BLACK STUDENTS": 437,
+        "Percentage": 5.73
     },
     {
-        Year: 1997,
-        Students: 7579
+        "Year": 1997,
+        "TOTAL STUDENTS": 7579,
+        "TOTAL BLACK STUDENTS": 448,
+        "Percentage": 5.91
     },
     {
-        Year: 1998,
-        Students: 7782
+        "Year": 1998,
+        "TOTAL STUDENTS": 7782,
+        "TOTAL BLACK STUDENTS": 441,
+        "Percentage": 5.67
     },
     {
-        Year: 1999,
-        Students: 7758
+        "Year": 1999,
+        "TOTAL STUDENTS": 7758,
+        "TOTAL BLACK STUDENTS": 426,
+        "Percentage": 5.49
     },
     {
-        Year: 2000,
-        Students: 7723
+        "Year": 2000,
+        "TOTAL STUDENTS": 7723,
+        "TOTAL BLACK STUDENTS": 438,
+        "Percentage": 5.67
     },
     {
-        Year: 2001,
-        Students: 7774
+        "Year": 2001,
+        "TOTAL STUDENTS": 7774,
+        "TOTAL BLACK STUDENTS": 428,
+        "Percentage": 5.51
     },
     {
-        Year: 2002,
-        Students: 7892
+        "Year": 2002,
+        "TOTAL STUDENTS": 7892,
+        "TOTAL BLACK STUDENTS": 442,
+        "Percentage": 5.6
     },
     {
-        Year: 2003,
-        Students: 7882
+        "Year": 2003,
+        "TOTAL STUDENTS": 7882,
+        "TOTAL BLACK STUDENTS": 458,
+        "Percentage": 5.81
     },
     {
-        Year: 2004,
-        Students: 8004
+        "Year": 2004,
+        "TOTAL STUDENTS": 8004,
+        "TOTAL BLACK STUDENTS": 475,
+        "Percentage": 5.93
     },
     {
-        Year: 2005,
-        Students: 8261
+        "Year": 2005,
+        "TOTAL STUDENTS": 8261,
+        "TOTAL BLACK STUDENTS": 502,
+        "Percentage": 6.08
     },
     {
-        Year: 2006,
-        Students: 8125
+        "Year": 2006,
+        "TOTAL STUDENTS": 8125,
+        "TOTAL BLACK STUDENTS": 492,
+        "Percentage": 6.06
     },
     {
-        Year: 2007,
-        Students: 8167
+        "Year": 2007,
+        "TOTAL STUDENTS": 8167,
+        "TOTAL BLACK STUDENTS": 487,
+        "Percentage": 5.96
     },
     {
-        Year: 2008,
-        Students: 8318
+        "Year": 2008,
+        "TOTAL STUDENTS": 8318,
+        "TOTAL BLACK STUDENTS": 501,
+        "Percentage": 6.02
     },
     {
-        Year: 2009,
-        Students: 8574
+        "Year": 2009,
+        "TOTAL STUDENTS": 8574,
+        "TOTAL BLACK STUDENTS": 442,
+        "Percentage": 5.16
     },
     {
-        Year: 2010,
-        Students: 8705
+        "Year": 2010,
+        "TOTAL STUDENTS": 8705,
+        "TOTAL BLACK STUDENTS": 461,
+        "Percentage": 5.3
     },
     {
-        Year: 2011,
-        Students: 8768
+        "Year": 2011,
+        "TOTAL STUDENTS": 8768,
+        "TOTAL BLACK STUDENTS": 470,
+        "Percentage": 5.36
     },
     {
-        Year: 2012,
-        Students: 8885
+        "Year": 2012,
+        "TOTAL STUDENTS": 8885,
+        "TOTAL BLACK STUDENTS": 488,
+        "Percentage": 5.49
     },
     {
-        Year: 2013,
-        Students: 8943
+        "Year": 2013,
+        "TOTAL STUDENTS": 8943,
+        "TOTAL BLACK STUDENTS": 513,
+        "Percentage": 5.74
     },
     {
-        Year: 2014,
-        Students: 9181
+        "Year": 2014,
+        "TOTAL STUDENTS": 9181,
+        "TOTAL BLACK STUDENTS": 550,
+        "Percentage": 5.99
     },
     {
-        Year: 2015,
-        Students: 9458
+        "Year": 2015,
+        "TOTAL STUDENTS": 9458,
+        "TOTAL BLACK STUDENTS": 565,
+        "Percentage": 5.97
     },
     {
-        Year: 2016,
-        Students: 9781
+        "Year": 2016,
+        "TOTAL STUDENTS": 9781,
+        "TOTAL BLACK STUDENTS": 573,
+        "Percentage": 5.86
     },
     {
-        Year: 2017,
-        Students: 10095
+        "Year": 2017,
+        "TOTAL STUDENTS": 10095,
+        "TOTAL BLACK STUDENTS": 590,
+        "Percentage": 5.84
     },
     {
-        Year: 2018,
-        Students: 10257
+        "Year": 2018,
+        "TOTAL STUDENTS": 10257,
+        "TOTAL BLACK STUDENTS": 621,
+        "Percentage": 6.05
     },
     {
-        Year: 2019,
-        Students: 10333
+        "Year": 2019,
+        "TOTAL STUDENTS": 10333,
+        "TOTAL BLACK STUDENTS": 672,
+        "Percentage": 6.5
     },
     {
-        Year: 2020,
-        Students: 9948
+        "Year": 2020,
+        "TOTAL STUDENTS": 9948,
+        "TOTAL BLACK STUDENTS": 700,
+        "Percentage": 7.04
     },
     {
-        Year: 2021,
-        Students: 10696
+        "Year": 2021,
+        "TOTAL STUDENTS": 10696,
+        "TOTAL BLACK STUDENTS": 791,
+        "Percentage": 7.4
     },
     {
-        Year: 2022,
-        Students: 11151
-    }
-]
-
-const totalBlackStudents = [
-    {
-        Year: 1980,
-        TotalBlackStudents: 382
-    },
-    {
-        Year: 1984,
-        TotalBlackStudents: 429
-    },
-    {
-        Year: 1986,
-        TotalBlackStudents: 464
-    },
-    {
-        Year: 1988,
-        TotalBlackStudents: 460
-    },
-    {
-        Year: 1990,
-        TotalBlackStudents: 446
-    },
-    {
-        Year: 1991,
-        TotalBlackStudents: 436
-    },
-    {
-        Year: 1992,
-        TotalBlackStudents: 443
-    },
-    {
-        Year: 1993,
-        TotalBlackStudents: 424
-    },
-    {
-        Year: 1994,
-        TotalBlackStudents: 436
-    },
-    {
-        Year: 1995,
-        TotalBlackStudents: 444
-    },
-    {
-        Year: 1996,
-        TotalBlackStudents: 437
-    },
-    {
-        Year: 1997,
-        TotalBlackStudents: 448
-    },
-    {
-        Year: 1998,
-        TotalBlackStudents: 441
-    },
-    {
-        Year: 1999,
-        TotalBlackStudents: 426
-    },
-    {
-        Year: 2000,
-        TotalBlackStudents: 438
-    },
-    {
-        Year: 2001,
-        TotalBlackStudents: 428
-    },
-    {
-        Year: 2002,
-        TotalBlackStudents: 442
-    },
-    {
-        Year: 2003,
-        TotalBlackStudents: 458
-    },
-    {
-        Year: 2004,
-        TotalBlackStudents: 475
-    },
-    {
-        Year: 2005,
-        TotalBlackStudents: 502
-    },
-    {
-        Year: 2006,
-        TotalBlackStudents: 492
-    },
-    {
-        Year: 2007,
-        TotalBlackStudents: 487
-    },
-    {
-        Year: 2008,
-        TotalBlackStudents: 501
-    },
-    {
-        Year: 2009,
-        TotalBlackStudents: 442
-    },
-    {
-        Year: 2010,
-        TotalBlackStudents: 461
-    },
-    {
-        Year: 2011,
-        TotalBlackStudents: 470
-    },
-    {
-        Year: 2012,
-        TotalBlackStudents: 488
-    },
-    {
-        Year: 2013,
-        TotalBlackStudents: 513
-    },
-    {
-        Year: 2014,
-        TotalBlackStudents: 550
-    },
-    {
-        Year: 2015,
-        TotalBlackStudents: 565
-    },
-    {
-        Year: 2016,
-        TotalBlackStudents: 573
-    },
-    {
-        Year: 2017,
-        TotalBlackStudents: 590
-    },
-    {
-        Year: 2018,
-        TotalBlackStudents: 621
-    },
-    {
-        Year: 2019,
-        TotalBlackStudents: 672
-    },
-    {
-        Year: 2020,
-        TotalBlackStudents: 700
-    },
-    {
-        Year: 2021,
-        TotalBlackStudents: 791
-    },
-    {
-        Year: 2022,
-        TotalBlackStudents: 834
+        "Year": 2022,
+        "TOTAL STUDENTS": 11151,
+        "TOTAL BLACK STUDENTS": 834,
+        "Percentage": 7.48
     }
 ]
 //Years array for independent axis on graph
@@ -314,41 +238,103 @@ const tickValuesYrs =
         2000, 2001, 2002, 2003, 2004,
         2005, 2006, 2007, 2008, 2009,
         2010, 2011, 2012, 2013, 2014,
-        2015, 2016, 2017, 2018, 2019,
-        2020, 2021, 2022]
+        2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
+    ]
+const tickValuesPercents = [
+    5.56, 6.04, 6.31, 6.04,
+    5.89, 5.79, 5.83, 5.54, 5.59, 5.81, 5.73, 5.91, 5.67, 5.49, 5.67, 5.51, 5.60, 5.81, 5.93, 6.08, 6.06, 5.96, 6.02, 5.16, 5.30, 5.36, 5.49, 5.74, 5.99, 5.97, 5.86, 5.84, 6.05, 6.50, 7.04, 7.40, 7.48
+]
 
+const tickValues2 = [1, 2, 3, 4, 5, 6, 7, 8.9, 10]
 const DataVis = () => {
-    return (
+    const [graph, setGraph] = useState('totalStudents')
+    if (graph === 'totalStudents') {
+        return (
+            <div>
+                <button onClick={() => {
+                    setGraph('totalPercentage')
+                    console.log("clicked")
+                }}>
+                    See bar graph
+                </button>
 
-        <VictoryChart
-            theme={VictoryTheme.material}
-            domain={{ x: [1980, 2022], y: [100, 12000] }}
-            style={{ parent: { maxWidth: "75%" } }}
-            width={1500}
-            height={1000}
-        >
-            {/* X axis for years */}
-            <VictoryAxis
-                tickValues={tickValuesYrs}
-            />
-            {/* Y axis for student count */}
-            <VictoryAxis dependentAxis />
+                <VictoryChart
+                    theme={VictoryTheme.material}
+                    style={{ parent: { maxWidth: "75%" } }}
+                    width={1500}
+                    height={1000}
+                    domain={{ x: [1980, 2022] }}
+                    domainPadding={{ x: 25 }}
+                >
+                    {/* X axis for years */}
+                    <VictoryAxis
+                        tickValues={tickValuesYrs}
+                        domain={{ x: [1980, 2022] }} />
+                    <VictoryAxis dependentAxis
+                        orientation='left'
+                        tickValues={tickValues2}
+                        domain={{ y: [5, 10] }} />
 
-            {/* Total students at Brown line */}
-            <VictoryBar
-                data={totalStudents}
-                x="Year"
-                y="Students"
-            />
-            {/* TotalBlackStudentss at Brown line */}
-            <VictoryBar
-                style={{ data: { fill: "#c43a31" } }}
-                data={totalBlackStudents}
-                x="Year"
-                y="TotalBlackStudents"
-            />
-        </VictoryChart>
-    )
+                    <VictoryLine
+                        data={totalBlackStudentsPercent}
+                        x="Year"
+                        y="Percentage" />
+                </VictoryChart>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <button onClick={() => {
+                    setGraph('totalStudents')
+                    console.log("clicked")
+                }}>
+                    SEE Percents
+                </button>
+                <VictoryChart
+                    theme={VictoryTheme.material}
+                    style={{ parent: { maxWidth: "75%" } }}
+                    width={1500}
+                    height={1000}
+                    domainPadding={{ x: 25 }}
+                >
+                    {/* X axis for years */}
+                    <VictoryAxis
+                        tickValues={tickValuesYrs}
+                        domain={{ x: [1980, 2022] }}
+                        label='Years (1980 - 2022)'
+                        labelComponent={
+                            <VictoryLabel
+                                dy={-1000}
+                            />
+
+                        }
+                    />
+
+                    {/* Y axis for student count */}
+                    <VictoryAxis dependentAxis
+                        orientation="left"
+                        domain={{ y: [100, 12000] }}
+                    />
+                    {/* Total students at Brown bar */}
+                    <VictoryBar
+                        data={totalBlackStudentsPercent}
+                        x="Year"
+                        y="TOTAL STUDENTS"
+                    />
+                    {/* Total Black Students at Brown bar */}
+                    <VictoryBar
+                        style={{ data: { fill: "#c43a31" } }}
+                        data={totalBlackStudentsPercent}
+                        x="Year"
+                        y="TOTAL BLACK STUDENTS"
+                    />
+                </VictoryChart>
+            </div>
+        )
+    }
+
 }
+
 
 export default DataVis
